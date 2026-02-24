dcf040d64461106b950a1251a409533c7b1292ed
zhiyuanzeng <2916363651@qq.com>
2026-02-24 11:17:10 +0800

    feat: add docker

--- nestdemo/src/app.module.ts
+++ nestdemo/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '@integration/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
